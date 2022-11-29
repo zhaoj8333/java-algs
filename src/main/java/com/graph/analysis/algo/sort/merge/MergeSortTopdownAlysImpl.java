5340417adddada148ec233fb186c0c1e1132b550
allen <18398759125@163.com>
2022-11-29 14:21:26 +0800

    MergeSortTopdownAlysImpl

--- src/main/java/com/graph/analysis/algo/sort/merge/MergeSortTopdownAlysImpl.java
+++ src/main/java/com/graph/analysis/algo/sort/merge/MergeSortTopdownAlysImpl.java
@@ -11,126 +11,133 @@ public class MergeSortTopdownAlysImpl<E extends Comparable<E>> extends MergeSort
         super(array, comparator);
     }
 
+    protected E[] aux0 = (E[]) new Comparable[array.length >> 1];
+
     @Override
     public void sort() {
-        if (array.length == 1) {
-            return;
-        }
-//        sort0(0, array.length);
-        sort(0, array.length - 1);
+
+        sort0(0, array.length);
+//        sort1(0, array.length - 1);
+
         Assertions.assertTrue(SortUtil.isSorted(array));
     }
 
     /**
-     * sort array[]: [begin, end), begin <= index < end
+     * sort: [begin, end)
+     *       [begin, mid)
+     *       [mid, end)
      */
     private void sort0(int begin, int end) {
         if (end - begin < 2) {
             return;
         }
-        int mid = (begin + end) >> 1;
+        int mid = (end + begin) >> 1;
         sort0(begin, mid);
         sort0(mid, end);
         merge0(begin, mid, end);
-//        fastMerge0(begin, mid, end);
     }
 
     /**
-     * merge: [begin, mid) and [mid, end)
+     * {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+     * {0, 1, 2, 3, 4, 5} {6, 7, 8, 9, 10}
      *
-     * 0                 9       13        18
-     *                   begin   mid       end
-     * - - - - - - - - - - - - - - - - - -      array
-     *                   li      le        re
-     *                           ri
-     *                   ai
-     *
-     *                   0
-     *                   - - - -                aux
-     *                   li      le
+     * merge:
+     *  [begin, mid) [mid, end)
      */
-    protected void merge0(int begin, int mid, int end) {
+    private void merge0(int begin, int mid, int end) {
         int li = 0, le = mid - begin;
         int ri = mid, re = end;
         int ai = begin;
         for (int i = li; i < le; i++) {
-            aux[i] = array[i + begin];
+            aux0[i] = array[begin + i];
             arrayAcc += 2;
         }
         while (li < le) {
-            if (ri < re && compareEntry(array[ri], aux[li]) < 0) {
+            if (ri < re && compareEntry(aux0[li], array[ri]) > 0) {
                 array[ai++] = array[ri++];
                 arrayAcc += 4;
             } else {
-                array[ai++] = aux[li++];
+                array[ai++] = aux0[li++];
                 arrayAcc += 2;
             }
         }
     }
 
+    private final E[] aux1 = (E[]) new Comparable[array.length];
+
     /**
-     * [0, array.length - 1]
-     *
-     * [begin, end]
-     * 
-     * add compareIndex(mid, mid + 1) > 0, may avoid call {@link #merge(int, int, int)}
+     * sort: [begin, end]
      */
-    private void sort(int begin, int end) {
+    private void sort1(int begin, int end) {
         if (end <= begin) {
             return;
         }
         int mid = (begin + end) >> 1;
-        sort(begin, mid);
-        sort(mid + 1, end);
-//        merge(begin, mid, end);
-        fastMerge(begin, mid, end);
+        sort1(begin, mid);
+        sort1(mid + 1, end);
+        merge10(begin, mid, end);
     }
 
     /**
-     *          0          5          10
-     * left[]:  E O R S T  X  A E L M P
-     *          m             n
-     *                    mid
+     * merge:
+     * [begin, mid], [mid + 1, end]
+     *
+     * array: {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+     *         begin       mid            end
+     *
+     * aux1:  {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+     *         i              j
      */
-    private void merge(int begin, int mid, int end) {
-        for (int i = begin; i <= end; i++) {
-            aux[i] = array[i];
+    private void merge1(int begin, int mid, int end) {
+        int i = begin, j = mid + 1;
+        for (int m = begin; m <= end; m++) {
+            aux1[m] = array[m];
             arrayAcc += 2;
         }
-        int li = begin, ri = mid + 1, ai = begin;
-        while (li <= mid && ri <= end) {
-            if (compareEntry(aux[li], aux[ri]) > 0) {
-                array[ai++] = aux[ri++];
+        for (int m = begin; m <= end; m++) {
+            if (i > mid) {
+                array[m] = aux1[j++];
+            } else if (j > end) {
+                array[m] = aux1[i++];
+            } else if (compareEntry(aux1[i], aux1[j]) > 0) {
+                arrayAcc += 2;
+                array[m] = aux1[j++];
             } else {
-                array[ai++] = aux[li++];
+                array[m] = aux1[i++];
             }
-            arrayAcc += 2;
-        }
-        while (li <= mid) {
-            array[ai++] = aux[li++];
             arrayAcc += 2;
         }
     }
 
-    private void fastMerge(int begin, int mid, int end) {
-        int auxIndex = begin;
-        for (int i = begin; i <= mid; i++) {
-            aux[auxIndex++] = array[i];
+    private E[] aux10 = (E[]) new Comparable[(array.length + 1) >> 1];
+
+    /**
+     * merge:
+     * [begin, mid], [mid + 1, end]
+     *
+     * array: {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+     *         begin       mid            end
+     *                        j
+     *
+     * aux10: {0, 1, 2, 3, 4}
+     *         i
+     */
+    private void merge10(int begin, int mid, int end) {
+        int li = 0, le = mid - begin;
+        int ri = mid + 1, ai = begin;
+        for (int m = li; m <= le; m++) {
+            aux10[m] = array[m + begin];
             arrayAcc += 2;
         }
-        for (int i = end; i > mid; i--) {
-            aux[auxIndex++] = array[i];
-            arrayAcc += 2;
-        }
-        int li = begin, ri = end, ai = begin;
-        while (li <= mid) {
-            if (compareEntry(aux[li], aux[ri]) <= 0) {
-                array[ai++] = aux[li++];
+        while (li <= le) {
+            if (ri <= end && compareEntry(array[ri], aux10[li]) < 0) {
+                arrayAcc += 2;
+                array[ai++] = array[ri++];
             } else {
-                array[ai++] = aux[ri--];
+                array[ai++] = aux10[li++];
             }
             arrayAcc += 2;
         }
-    }
+   }
 
 }
